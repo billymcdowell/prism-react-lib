354ceaeda9ef62f6ad5e90be8d257e2e2e3db076
BillyMcDowell <75757263+billymcdowell@users.noreply.github.com>
2025-12-01 10:33:39 +0000

    add changelog to storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,8 +2,9 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../src/stories/docs/Introduction.mdx",
+    "../src/stories/**/*.mdx",
+    "../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-docs",
